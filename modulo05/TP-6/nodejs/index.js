42aed51c4563f4248733bad3b00603531921d38b
JonathanAyala01 <ayala3438@gmail.com>
2023-08-31 09:35:37 -0300

    TP-06-FULLTACK

---
+++ modulo05/TP-6/nodejs/index.js
@@ -0,0 +1,9 @@
+//Eje4- b) 
+
+console.log('Hola soy un programa')
+
+//Eje5- b)
+
+const operacion = require("./programa")
+// LLamado al objeto operacion
+operacion.sumar(42, 35);
---
+++ modulo05/TP-6/nodejs/programa.js
@@ -0,0 +1,9 @@
+// Declaro la función sumar y paso como parametro 2 numeros
+function sumar(var1, var2) {
+    resultado = var1 + var2;
+    console.log("La suma es: "+resultado);
+    }
+    // Declaro la explotación de la función
+    module.exports = {
+    "sumar": sumar
+    }
